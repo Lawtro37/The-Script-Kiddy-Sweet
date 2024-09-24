fb843a13dc85c58f529fb8e092e0a72df006bf27
Lawtro37 <98205608+Lawtro37@users.noreply.github.com>
2024-09-24 14:43:33 +1000

    Update injectScr.js

--- modules/injectScr.js
+++ modules/injectScr.js
@@ -1,22 +1,21 @@
-function injectScript() {
-    new ThemedWindow(100, 100, 600, 500, true, true, true, true, true, 'Script Injector', `
-        <textarea id="scriptText" style="width: 100%; height: 200px;"></textarea>
-        <button id="injectScript">Inject</button>
-    `);
-    document.getElementById('Inject Script').addEventListener('click', () => {
-        let scriptText = document.getElementById('scriptText').value;
-        try {
-            if(scriptText !== '') {
-                let script = document.createElement('script');
-                if(scriptText.includes('http')) {
-                    script.src = scriptText;
-                } else {
-                    script.innerHTML = scriptText;
-                }
-                document.body.appendChild(script);
+
+new ThemedWindow(100, 100, 600, 500, true, true, true, true, true, 'Script Injector', `
+    <textarea id="scriptText" style="width: 100%; height: 200px;"></textarea>
+    <button id="injectScript">Inject</button>
+`);
+document.getElementById('Inject Script').addEventListener('click', () => {
+    let scriptText = document.getElementById('scriptText').value;
+    try {
+        if(scriptText !== '') {
+            let script = document.createElement('script');
+            if(scriptText.includes('http')) {
+                script.src = scriptText;
+            } else {
+                script.innerHTML = scriptText;
             }
-        } catch (e) {
-            document.getElementById('injectScript').parentElement.appendChild(document.createElement('div')).textContent = e;
+            document.body.appendChild(script);
         }
-    });
-}
+    } catch (e) {
+        document.getElementById('injectScript').parentElement.appendChild(document.createElement('div')).textContent = e;
+    }
+});
